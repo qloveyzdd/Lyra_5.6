208134108cad95ff075773a33b1c92cf6d7b3ed5
qloveyzdd <84934701@qq.com>
2025-10-15 17:32:21 +0800

    AssetManager类 PawnData加载 调试

--- Source/LyraGame/System/LyraAssetManager.cpp
+++ Source/LyraGame/System/LyraAssetManager.cpp
@@ -5,9 +5,18 @@
 #include "LyraAssetManagerStartupJob.h"
 #include "LyraGameData.h"
 #include "LyraLogChannels.h"
+#include "Editor/Kismet/Internal/Blueprints/BlueprintDependencies.h"
+#include "EditorState/EditorState.h"
 
 //一个约定的Bundles的命名
 const FName FLyraBundles::Equipped("Equipped");
+
+//通过命令行调用这个方法 打印加载的资产
+static FAutoConsoleCommand CVarDumpLoadedAssets(
+	TEXT("Lyra,DumpLoadedAssets"),
+	TEXT("Shows all assets that were loaded via the asset manager and are currently in memory."),
+	FConsoleCommandDelegate::CreateStatic(ULyraAssetManager::DumpLoadedAssets)
+);
 
 //添加一个任务到容器里面，这个任务就是传递过来的JobFunc，并用Lambda包了一层
 //入参是函数，函数权重
@@ -41,9 +50,57 @@ ULyraAssetManager& ULyraAssetManager::Get()
 	return *NewObject<ULyraAssetManager>();
 }
 
+void ULyraAssetManager::DumpLoadedAssets()
+{
+	UE_LOG(LogLyra, Log, TEXT("=========== Start Dumping Loaded Assets ==========="));
+
+	for (const UObject* LoadedAsset : Get().LoadedAssets)
+	{
+		UE_LOG(LogLyra, Log, TEXT("	%s"), *GetNameSafe(LoadedAsset));
+	}
+
+	UE_LOG(LogLyra, Log, TEXT("... %d assets in loaded pool"), Get().LoadedAssets.Num());
+	UE_LOG(LogLyra, Log, TEXT("=========== Finish Dumping Loaded Assets ==========="));
+}
+
 const ULyraPawnData* ULyraAssetManager::GetDefaultPawnData() const
 {
+	return GetAsset(DefaultPawnData);
+}
+
+UObject* ULyraAssetManager::SynchronousLoadAsset(const FSoftClassPath& AssetPath)
+{
+	if (AssetPath.IsValid())
+	{
+		TUniquePtr<FScopeLogTime> LogTimePtr;
+		//通过命名行确认是否需要打印该日志
+		if (ShouldLogAssetLoads())
+		{
+			LogTimePtr = MakeUnique<FScopeLogTime>(
+				*FString::Printf(TEXT("Synchronously loaded asset [%s]"), *AssetPath.ToString()),
+				nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+		if (UAssetManager::IsInitialized())
+		{
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath, false);
+		}
+	}
 	return nullptr;
+}
+
+bool ULyraAssetManager::ShouldLogAssetLoads()
+{
+	static bool bLogAssetLoads = FParse::Param(FCommandLine::Get(),TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+void ULyraAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	if (ensureAlways(Asset))
+	{
+		FScopeLock LoadedAssetsLock(&LoadedAssetsCritical);
+		LoadedAssets.Add(Asset);
+	}
 }
 
 void ULyraAssetManager::StartInitialLoading()
@@ -75,6 +132,97 @@ void ULyraAssetManager::StartInitialLoading()
 void ULyraAssetManager::PreBeginPIE(bool bStartSimulate)
 {
 	Super::PreBeginPIE(bStartSimulate);
+
+	{
+		FScopedSlowTask SlowTask(0,NSLOCTEXT("LyraEditor","BeginLoadingPIEData","Loading PIE Data"));
+		const bool bShowCancelButton = false;
+		const bool bAllowInPIE = true;
+		SlowTask.MakeDialog(bShowCancelButton,bAllowInPIE);
+
+		//这里没有就崩溃
+		const ULyraGameData& LocalGameDataCommon = GetGameData();
+
+		//有意安排在获取游戏数据之后进行，以避免将游戏数据的处理时间计入此计时器
+		SCOPE_LOG_TIME_IN_SECONDS(TEXT("PreBegingPIE asset preloading complete"),nullptr);
+
+		//可以预先加载我们在此处所使用体验所需的任何其他内容
+		//可以参考我们在EditorEngine中的操作
+	}
+}
+
+UPrimaryDataAsset* ULyraAssetManager::LoadGameDataOfClass(TSubclassOf<UPrimaryDataAsset> DataClass,
+                                                          const TSoftObjectPtr<UPrimaryDataAsset>& DataClassPath,
+                                                          FPrimaryAssetType PrimaryAssetType)
+{
+	UPrimaryDataAsset* Asset = nullptr;
+
+	//用作性能分析
+	DECLARE_SCOPE_CYCLE_COUNTER(TEXT("Loading GameData Object"), STAT_GameDate, STATGROUP_LoadTime);
+
+	//加载路径不能为空
+	if (!DataClassPath.IsNull())
+	{
+#if WITH_EDITOR
+		//一个表示工作量的划分单元，该单元被划分为若干部分
+		//在每一个函数的顶部使用一个范围块，以便向缓慢操作的用户提供准确的进度反馈
+		FScopedSlowTask SlowTask(0, FText::Format(
+			                         NSLOCTEXT("LyraEditor", "BeginLoadingGameDataTask", "Loading GameData {0}"),
+			                         FText::FromName(DataClass->GetFName())));
+
+		const bool bShowCancelButton = false;
+
+		const bool bAllowInPIE = true;
+
+		SlowTask.MakeDialog(bShowCancelButton, bAllowInPIE);
+#endif
+
+		UE_LOG(LogLyra, Log, TEXT("Loading GameData: %s ..."), *DataClassPath.ToString());
+
+		//指定计时打印日志的宏
+		SCOPE_LOG_TIME_IN_SECONDS(TEXT("	... GameData loaded!"), nullptr);
+
+		//在编辑器中可以对该函数进行递归操作，因为它是根据需求从“PostLoad”阶段被调用的，所以在此情况下必须强制进行主资产的同步加载，并对其余部分进行异步加载。
+
+		if (GIsEditor)
+		{
+			//先加载主资产
+			Asset = DataClassPath.LoadSynchronous();
+
+			//加载指定类型的全部资源，适用于烘培操作
+			LoadPrimaryAssetsWithType(PrimaryAssetType);
+		}
+		else
+		{
+			TSharedPtr<FStreamableHandle> Handle = LoadPrimaryAssetsWithType(PrimaryAssetType);
+
+			if (Handle.IsValid())
+			{
+				//直到所需资源加载完成才会停止，这会将所请求的资源推至优先级列表的首位
+				//但不会清理所有异步加载操作，通常会比调用LoadObject函数完成的更快。
+				Handle->WaitUntilComplete(0.0f, false);
+
+				//返回所请求资产列表中的首个资产（若该资产已成功加载），若该资产加载失败，则此操作将失败
+				Asset = Cast<UPrimaryDataAsset>(Handle->GetLoadedAsset());
+			}
+		}
+	}
+
+	if (Asset)
+	{
+		//添加到缓存中 Key值是类类型 value值是指针
+		GameDataMap.Add(DataClass, Asset);
+	}
+	else
+	{
+		//任何游戏数据资产的加载都不得失败，否则将会导致一些不易诊断的软性故障
+		UE_LOG(LogLyra, Fatal,
+		       TEXT(
+			       "Failed to load GameData asset at %s. Type %s. This is not recoverable and likely means you do not have the correct data to run %s."
+		       ), *DataClassPath.ToString(), *PrimaryAssetType.ToString(), FApp::GetProjectName());
+	}
+
+
+	return Asset;
 }
 
 void ULyraAssetManager::DoAllStartupJobs()
@@ -169,5 +317,5 @@ void ULyraAssetManager::UpdateInitialGameContentLoadPercent(float GameContentPer
 
 const class ULyraGameData& ULyraAssetManager::GetGameData()
 {
-	return *NewObject<ULyraGameData>();
+	return GetOrLoadTypedGameData<ULyraGameData>(LyraGameDataPath);
 }
--- Source/LyraGame/System/LyraAssetManager.h
+++ Source/LyraGame/System/LyraAssetManager.h
@@ -27,13 +27,56 @@ public:
 	//返回资产管理器的单例对象
 	static LYRAGAME_API ULyraAssetManager& Get();
 
+	//TSoftObjectPtr是通用FSoftObjectPtr的模板化封装类，可用于UProperties中，
+
+	//返回由TSoftObjectPtr引用的资产，如果该资产尚未加载，则会同步加载该资产。
+	template <typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	//返回由TSoftClassPtr指向的子类。如果该资产尚未加载，则会同步进行加载操作。
+
+	//返回由TSoftClassPtr指向的子类，如果该资产尚未加载，则会同步进行加载操作。
+	template <typename AssetType>
+	static TSubclassOf<AssetType>* GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+
+	//记录当前由资产管理器加载并跟踪的所有资产信息
+	//可以通过命令行调用
+	static LYRAGAME_API void DumpLoadedAssets();
+
+	
 	//获取游戏数据
 	LYRAGAME_API const class ULyraGameData& GetGameData();
 
 	//获取默认的玩家数据
-	LYRAGAME_API const ULyraPawnData* GetDefaultPawnData() const;
+	const ULyraPawnData* GetDefaultPawnData() const;
 
 protected:
+	//获取或加载指定的游戏数据
+	template <typename GameDataClass>
+	const GameDataClass& GetOrLoadTypedGameData(const TSoftObjectPtr<GameDataClass>& DataPath)
+	{
+		//如果已经缓存了直接读取
+		if (const TObjectPtr<UPrimaryDataAsset>* pResult = GameDataMap.Find(GameDataClass::StaticClass()))
+		{
+			return *CastChecked<GameDataClass>(*pResult);
+		}
+		//如有需要则进行阻塞式加载
+		return *CastChecked<GameDataClass>(LoadGameDataOfClass(
+			GameDataClass::StaticClass(),
+			DataPath,
+			GameDataClass::StaticClass()->GetFName()));
+	}
+
+	//同步加载资产
+	static LYRAGAME_API UObject* SynchronousLoadAsset(const FSoftClassPath& AssetPath);
+
+	//读取命令行参数，是否应该打印资产加载日志
+	static LYRAGAME_API bool ShouldLogAssetLoads();
+
+	//一种线程安全的添加已加载资源到内存中的方法
+	LYRAGAME_API void AddLoadedAsset(const UObject* Asset);
+
 	//开始初始加载，由”初始化对象引用“函数调用
 	virtual void StartInitialLoading() override;
 
@@ -41,6 +84,12 @@ protected:
 #if WITH_EDITOR
 	LYRAGAME_API virtual void PreBeginPIE(bool bStartSimulate) override;
 #endif
+
+	//一种主要的资产类型，内部以FName格式表示，并且可以自动进行双向转换
+	//此设置的存在是为了让蓝图API能够明白者不是一个普通的FName类型
+	LYRAGAME_API UPrimaryDataAsset* LoadGameDataOfClass(TSubclassOf<UPrimaryDataAsset> DataClass,
+	                                                    const TSoftObjectPtr<UPrimaryDataAsset>& DataClassPath,
+	                                                    FPrimaryAssetType PrimaryAssetType);
 
 protected:
 	//所需的全局游戏数据资源
@@ -70,5 +119,61 @@ private:
 	LYRAGAME_API void UpdateInitialGameContentLoadPercent(float GameContentPercent);
 
 	//启动时要执行的任务列表，用于跟踪启动过程的进度。
-	TArray<class FLyraAssetManagerStartupJob> StartupJobs;
+	TArray<struct FLyraAssetManagerStartupJob> StartupJobs;
+
+private:
+	//资源已由资源管理器加载并进行跟踪
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	//用于在修改加载资源列表时进行范围锁定
+	FCriticalSection LoadedAssetsCritical;
 };
+
+template <typename AssetType>
+AssetType* ULyraAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath.ToString()));
+		ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPath.ToString());
+	}
+
+	if (LoadedAsset && bKeepInMemory)
+	{
+		//已添加至已加载资源列表
+		Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+	}
+
+	return LoadedAsset;
+}
+
+template <typename AssetType>
+TSubclassOf<AssetType>* ULyraAssetManager::GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<AssetType>(SynchronousLoadAsset(AssetPath.ToString()));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset class [%s]"), *AssetPath.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			//已添加至已加载资源列表
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
